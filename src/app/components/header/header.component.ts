10e2af96c27a89c389e9404c5dd08f4b7b15196f
Jean Fraga <jeanfragoli395@gmail.com>
2023-11-27 14:35:46 -0300

    feat: create header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonMenuComponent } from '../button-menu/button-menu.component';
+import { CurtainMenuComponent } from '../curtain-menu/curtain-menu.component';
+import { dataHeaderMenuLinks, DataHeaderMenuLinks } from '../../../data/data-header-menu-links';
+import { ButtonComponent } from '../link-button/link-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonMenuComponent, CurtainMenuComponent, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isOpeCurtainMenu: boolean = false;
+  dataLinks: DataHeaderMenuLinks[] = dataHeaderMenuLinks;
+  public toggleCurtainMenu () {
+    this.isOpeCurtainMenu = !this.isOpeCurtainMenu;
+  };
 
+  public closeMenu () {
+    this.isOpeCurtainMenu = false;
+  }
 }
