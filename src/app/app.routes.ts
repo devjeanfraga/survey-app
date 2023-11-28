0f88f30f8d52a8c29f15c9fa8277afb0fef63e67
Jean Fraga <jeanfragoli395@gmail.com>
2023-11-28 10:31:59 -0300

    chore: config  routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'sign-up', component: SignupComponent},
+  { path: 'login', component: LoginComponent}
+];
+
+
+
